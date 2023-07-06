b23811939185a4cb03b24c8a47bedc811e3b413a
314u <pyuloko@outlook.com>
2023-07-06 16:41:35 -0300

    Finalizada V1

--- js/script.js
+++ js/script.js
@@ -2,10 +2,21 @@ var estados = ["/img/trilhascheckbox/1.png", "/img/trilhascheckbox/2.png", "/img
 var indiceEstado = 0;
 function alterar(componente) {
     var estadoImg = componente.querySelector(".estado");
-    console.log(estadoImg.alt);
     var indiceEstado = estados.indexOf("/img/trilhascheckbox/" + estadoImg.alt + ".png");
-    console.log(indiceEstado);
     indiceEstado = (indiceEstado + 1) % estados.length;
     estadoImg.src = estados[indiceEstado];
     estadoImg.alt = (indiceEstado + 1);
+}
+
+function radiobutton(componente) {
+    if (componente.value == "1") {
+        console.log("marcado");
+        componente.checked = false;
+        componente.value = "0";
+    }
+    else {
+        console.log(" não marcado");
+        componente.checked = true;
+        componente.value = "1";
+    }
 }
