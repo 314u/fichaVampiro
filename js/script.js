4a39fd0c0d140b139cadc7d2256d4b1dcaf70b97
314u <pyuloko@outlook.com>
2023-07-27 13:08:36 -0300

    Corrigido os estados de fome e humanidade

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-var estados = ["/img/trilhascheckbox/1.png", "/img/trilhascheckbox/2.png", "/img/trilhascheckbox/3.png", "/img/trilhascheckbox/4.png"];
-var indiceEstado = 0;
 function alterar(componente) {
+    var estados = ["/img/trilhascheckbox/1.png", "/img/trilhascheckbox/2.png", "/img/trilhascheckbox/3.png", "/img/trilhascheckbox/4.png"];
+    var indiceEstado = 0;
+    var estadoImg = componente.querySelector(".estado");
+    var indiceEstado = estados.indexOf("/img/trilhascheckbox/" + estadoImg.alt + ".png");
+    indiceEstado = (indiceEstado + 1) % estados.length;
+    estadoImg.src = estados[indiceEstado];
+    estadoImg.alt = (indiceEstado + 1);
+}
+function alterarFome(componente) {
+    var estados = ["/img/trilhascheckbox/1.png", "/img/trilhascheckbox/4.png"];
+    var indiceEstado = 0;
+    var estadoImg = componente.querySelector(".estado");
+    var indiceEstado = estados.indexOf("/img/trilhascheckbox/" + estadoImg.alt + ".png");
+    indiceEstado = (indiceEstado + 1) % estados.length;
+    estadoImg.src = estados[indiceEstado];
+    estadoImg.alt = (indiceEstado + 1);
+}
+function alterarHumanidade(componente) {
+    var estados = ["/img/trilhascheckbox/1.png", "/img/trilhascheckbox/2.png", "/img/trilhascheckbox/4.png"];
+    var indiceEstado = 0;
     var estadoImg = componente.querySelector(".estado");
     var indiceEstado = estados.indexOf("/img/trilhascheckbox/" + estadoImg.alt + ".png");
     indiceEstado = (indiceEstado + 1) % estados.length;
